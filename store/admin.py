f9572a4028cbcdfd38d7b65cc0772560a6543e5b
oshikadhungana7 <oshikadhungana798@gmail.com>
2022-01-29 08:24:12 -0800

    admin

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
 
+from store.models import Customer, Order, OrderItem, Product, ShippingAddress
+
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
+
+
 
 
