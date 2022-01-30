d2a7af408d495f0037aac588c7800ee80028c0b3
oshikadhungana7 <oshikadhungana798@gmail.com>
2022-01-30 08:37:21 -0800

    views

--- store/views.py
+++ store/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
-
+from .models import *
 
 # Create your views here.
+
+from django.shortcuts import render
+
+def store(request):
+	products = Product.objects.all()
+	context = {'products' :products}
+	return render(request, 'store/store.html', context)
+
+def cart(request):
+    context  =  {}
+    return render(request, 'store/cart.html', context)
+	
+
+
+
+
+
+def checkout(request):
+	context = {}
+	return render(request, 'store/checkout.html', context)
 
