f322f4adce970cd60b12206f5026bb6e060638d3
oshikadhungana7 <oshikadhungana798@gmail.com>
2022-01-31 09:08:34 -0800

    __init__

--- store/views.py
+++ store/views.py
@@ -14,11 +14,6 @@ def cart(request):
     context  =  {}
     return render(request, 'store/cart.html', context)
 	
-
-
-
-
-
 def checkout(request):
 	context = {}
 	return render(request, 'store/checkout.html', context)
