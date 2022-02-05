40c24b75f5a6638ee95c6add12f235867e5106ca
oshikadhungana7 <oshikadhungana798@gmail.com>
2022-02-04 20:01:15 -0800

    commit

--- store/models.py
+++ store/models.py
@@ -1,21 +1,23 @@
 from email.mime import image
 from itertools import product
 from math import fabs
+from re import T
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Customer(models.Model):
-    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200)
-    def __str__(Self):
-         return Self.name
+    email = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+         return self.name
 
 
-class Product(models .Model):
-    name = models . CharField(max_length=200)
-    price = models . FloatField()
-    digital = models.BooleanField(default=False, null=True, blank=True)
+class Product(models.Model):
+    name = models.CharField(max_length=200,null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=False)
     image = models.ImageField(null=True, blank=True)
     def __str__(self) :
         return self.name
@@ -29,26 +31,26 @@ class Product(models .Model):
 
 
 class Order(models.Model):
-    Customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
-    complete = models.BooleanField(default=False)
-    transaction_id = models.CharField(max_length=100, null=True)
+    complete = models.BooleanField(default=False, null=True,blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
     def __str__(self) :
         return str(self.id) 
 
 class OrderItem(models.Model):
-   Product = models . ForeignKey(Product, on_delete=models.SET_NULL, null=True)                        
-   Order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+   Product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True,null=True)                        
+   order = models.ForeignKey(Order, on_delete=models.SET_NULL,blank=True,null=True)
    quantity = models.IntegerField(default=0, null=True, blank=True)
    date_added = models.DateTimeField(auto_now_add=True)
 
 class ShippingAddress(models .Model):
-    Customer = models .ForeignKey(Customer , on_delete=models.SET_NULL, null=True)
-    Order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
-    address = models.CharField(max_length=200, null=False)
-    city = models.CharField(max_length=200, null=False)
-    State = models.CharField(max_length=200, null=False)
-    zipcode = models.CharField(max_length=200, null=False)
+    customer = models .ForeignKey(Customer , on_delete=models.SET_NULL, null=True,blank=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True,blank=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    State = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
     def __str__(self) :
         return self.address
--- store/views.py
+++ store/views.py
@@ -1,3 +1,5 @@
+from multiprocessing import context
+from typing import ItemsView
 from django.shortcuts import render
 from .models import *
 
@@ -11,9 +13,15 @@ def store(request):
 	return render(request, 'store/store.html', context)
 
 def cart(request):
-    context  =  {}
-    return render(request, 'store/cart.html', context)
-	
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		items = []	
+	context =	{'items' :items}
+	return render (request, 'store/cart.html', context)
+       
 def checkout(request):
 	context = {}
 	return render(request, 'store/checkout.html', context)
